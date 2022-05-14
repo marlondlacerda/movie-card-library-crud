5b4fa4f7ca52494ddafcdb818a0a3503a42bd8da
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-13 22:16:52 -0300

    refactor: 💡 update index files

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+export { default as Loading } from './Loading';
+export { default as MovieForm } from './MovieForm';
+export { default as MovieCard } from './MovieCard';
+export { default as Header } from './Header';
+export { default as FeaturedMovie } from './FeaturedMovie';
+export { default as SearchBar } from './SearchBar';
+export { default as MovieList } from './MovieList';
+export { default as Footer } from './Footer';
