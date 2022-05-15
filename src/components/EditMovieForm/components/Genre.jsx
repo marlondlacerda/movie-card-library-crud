fa48202ed84f42ad93d2246698a7081e3e2a46e7
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-14 23:10:40 -0300

    feat: 🎸 create GenreInput for EditMovieForm

---
+++ src/components/EditMovieForm/components/Genre.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Genre = ({ value, onChange }) => (
+  <div className="edit-container-genre">
+    <label htmlFor="genres">
+      Gênero
+    </label>
+    <select
+      name="genres"
+      className="edit-select"
+      id="genres"
+      value={ value }
+      onChange={ onChange }
+      multiple
+    >
+      <option value="Action">Ação</option>
+      <option value="Adventure">Aventura</option>
+      <option value="Fantasy">Fantasia</option>
+      <option value="Anime"> Anime</option>
+      <option value="Sci-fi">Sci-fi</option>
+      <option value="Crime">Crime</option>
+      <option value="Drama">Drama</option>
+      <option value="Mystery">Suspense</option>
+      <option value="Terror">Terror</option>
+    </select>
+  </div>
+);
+
+Genre.propTypes = {
+  value: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Genre;
