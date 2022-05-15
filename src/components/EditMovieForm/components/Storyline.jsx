7d60fe067cdb2f9e4478bb90915e4d61a09f6793
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-14 23:11:40 -0300

    feat: 🎸 create StorylineInput for EditMovieForm

---
+++ src/components/EditMovieForm/components/Storyline.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Storyline = ({ value, onChange }) => (
+  <div className="edit-title-box">
+    <textarea
+      name="storyline"
+      value={ value }
+      onChange={ onChange }
+      placeholder=""
+      onFocus={ (e) => { e.target.placeholder = 'Escreva a Sinopse do Filme!'; } }
+      onBlur={ (e) => { e.target.placeholder = ''; } }
+      required
+    />
+    <label htmlFor="storyline">
+      Sinopse
+    </label>
+  </div>
+);
+
+Storyline.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Storyline;
