299224baff50d205d38fc3c9bd1f64a265542d58
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-14 23:12:57 -0300

    refactor: 💡 transform CheckBoxInput class to a function

--- src/components/SearchBar/components/CheckBoxInput.jsx
+++ src/components/SearchBar/components/CheckBoxInput.jsx
@@ -3,29 +3,23 @@ import PropTypes from 'prop-types';
 
 import './CheckBoxInput.css';
 
-class CheckBoxInputLabel extends React.Component {
-  render() {
-    const { bookMarkedOnly, onBookMarkedChange } = this.props;
-
-    return (
-      <label
-        htmlFor="bookMarkedOnly"
-        data-testid="checkbox-input-label"
-        className="label-checkbox"
-      >
-        <input
-          type="checkbox"
-          data-testid="checkbox-input"
-          name="bookMarkedOnly"
-          checked={ bookMarkedOnly }
-          onChange={ onBookMarkedChange }
-          className="switch"
-        />
-        Mostrar somente favoritos
-      </label>
-    );
-  }
-}
+const CheckBoxInputLabel = ({ bookMarkedOnly, onBookMarkedChange }) => (
+  <label
+    htmlFor="bookMarkedOnly"
+    data-testid="checkbox-input-label"
+    className="label-checkbox"
+  >
+    <input
+      type="checkbox"
+      data-testid="checkbox-input"
+      name="bookMarkedOnly"
+      checked={ bookMarkedOnly }
+      onChange={ onBookMarkedChange }
+      className="switch"
+    />
+    Mostrar somente favoritos
+  </label>
+);
 
 CheckBoxInputLabel.propTypes = {
   bookMarkedOnly: PropTypes.bool.isRequired,
