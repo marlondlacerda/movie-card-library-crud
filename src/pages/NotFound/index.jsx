11a7936069db4377cac5c9b4ea92d53efe42f276
Marlon Lacerda <marlon.desagitario@hotmail.com>
2022-05-16 17:46:05 -0300

    feat: 🎸 create notFound page

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+
+import notFound from './notfound.png';
+import './NotFound.css';
+
+const NotFound = () => {
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  if (shouldRedirect) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div data-testid="404-error" className="notfound-container">
+      <div className="notfound-box">
+        <img src={ notFound } alt="notfound" />
+        <h1 className="notfound-404">
+          4
+          <span>0</span>
+          4
+        </h1>
+        <h2 className="notfound-text">page not found</h2>
+        <div className="button-container">
+          <button
+            data-testid="send-button"
+            type="button"
+            onClick={ () => setShouldRedirect(true) }
+          >
+            <span />
+            <span />
+            <span />
+            <span />
+            go home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
